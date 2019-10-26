76f24895946ee6c760a9f3a40a17f43490c12095
ivamluz <ivam.santos@gmail.com>
2019-10-26 20:27:49 -0300

    Create DnsRecords resource and DnsRecordsController skeleton

---
+++ server/app/controllers/dns_records_controller.rb
@@ -0,0 +1,37 @@
+class DnsRecordsController < ApplicationController
+  def show
+    @result = {
+      :total_records => 1,
+      :records => [
+        {
+          :id => 1,
+          :ip_address => "1.1.1.1"
+        },
+       ],
+      :related_hostnames => [
+        {
+          :hostname => "lorem.com",
+          :count => 5
+        },
+      ],
+    }
+
+    render json: @result.to_json
+  end
+
+  def create
+    @result = {
+      :id => 1,
+    }
+
+    render json: @result.to_json
+  end
+
+  def update
+    raise NotImplementedError
+  end
+
+  def destroy
+    raise NotImplementedError
+  end
+end
---
+++ server/test/controllers/dns_records_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DnsRecordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
