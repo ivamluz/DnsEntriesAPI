81416f1a1e74ac60517e9f78acfeb111cd8dcf1d
ivamluz <ivam.santos@gmail.com>
2019-10-26 20:43:11 -0300

    Add basic test to atest DnsControllerTest works

--- server/test/controllers/dns_records_controller_test.rb
+++ server/test/controllers/dns_records_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class DnsRecordsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "returns hard-coded value" do
+    get dns_records_url
+    assert_response :success
+
+    assert_equal "application/json", @response.media_type
+    
+    @expected_response = {
+      :total_records => 1,
+      :records => [
+        {
+          :id => 1,
+          :ip_address => "1.1.1.1"
+        },
+       ],
+      :related_hostnames => [
+        {
+          :hostname => "lorem.com",
+          :count => 5
+        },
+      ],
+    }
+
+    assert_equal @expected_response.to_json, @response.body
+  end
 end
