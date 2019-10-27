a356f240c7a104313283691bf330bff25de2b7d4
ivamluz <ivam.santos@gmail.com>
2019-10-27 13:33:49 -0300

    Implement unit tests for DnsRecord model

--- server/app/models/dns_record.rb
+++ server/app/models/dns_record.rb
@@ -1,3 +1,7 @@
+require 'resolv'
+
 class DnsRecord < ApplicationRecord
   has_and_belongs_to_many :hostnames
+
+  validates :ip, presence: true, format: { with: Resolv::IPv4::Regex }
 end
--- server/test/models/dns_record_test.rb
+++ server/test/models/dns_record_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class DnsRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should accept valid IPs' do
+    valid_ips = [
+      '0.0.0.0',
+      '255.255.255.255',
+      '192.169.0.1',
+      '8.8.8.8'
+    ]
+
+    valid_ips.each do |ip|
+      record = DnsRecord.new(ip: ip)    
+      assert record.valid?
+    end
+  end
+
+  test 'Should not accept null IPs' do
+    record = DnsRecord.new()
+    assert record.invalid?
+  end
+
+  test 'Should not accept empty IPs' do
+    record = DnsRecord.new(ip: '   ')
+    assert record.invalid?
+  end
+
+  test 'Should not accept invalid IP formats' do
+    invalid_ips = {
+      "1" => "Invalid mask",
+      "1." => "Invalid mask",
+      "1.1" => "Invalid mask",
+      "1.1." => "Invalid mask",
+      "1.1.1" => "Invalid mask",
+      "1.1.1." => "Invalid mask",
+      "..." => "Invalid mask",
+      "1...1" => "Invalid mask",
+      "258.258.258.258" => "Invalid value",
+    }
+
+    invalid_ips.each do |ip, reason|
+      record = DnsRecord.new(ip: ip)
+      assert record.invalid?, "#{reason}: #{ip}"
+    end
+  end
 end
