7c150feda69ece7db5c8b3c20bf56e8a3f044527
ivamluz <ivam.santos@gmail.com>
2019-10-27 16:10:22 -0300

    Implement unit tests for Hostname model

--- server/app/models/hostname.rb
+++ server/app/models/hostname.rb
@@ -1,3 +1,7 @@
+require 'uri'
+
 class Hostname < ApplicationRecord
   has_and_belongs_to_many :dns_records
+
+  validates :hostname, presence: true, format: { with: Regexp.new("\\A#{URI::PATTERN::HOSTNAME}\\z") }
 end
--- server/test/models/hostname_test.rb
+++ server/test/models/hostname_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class HostnameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should accept valid fully qualified domains' do
+    hostnames = [
+      'www.google.com',
+      'test.foob.ar'
+    ]
+
+    hostnames.each do |h|
+      hostname = Hostname.new(hostname: h)
+      assert hostname.valid?
+    end
+  end
+
+  test 'Should accept naked domains' do
+    hostname = Hostname.new(hostname: 'google.com')
+    assert hostname.valid?
+  end
+
+  test 'Should not accept null hostnames' do
+    hostname = Hostname.new()
+    assert hostname.invalid?
+  end
+
+  test 'Should not accept empty hostnames' do
+    hostname = Hostname.new(hostname: '   ')
+    assert hostname.invalid?
+  end
+
+  test 'Should not accept invalid hostnames' do
+    hostnames = [
+      '!jkfd.com',
+      '@mfd.com',
+      'jkfd@jkfdkd.com',
+      'http://www.foo.com',
+      'www.foo.com/bar'
+    ]
+
+    hostnames.each do |h|
+      hostname = Hostname.new(hostname: h)
+
+      assert hostname.invalid?, "#{h} should be invalid."
+    end
+  end
 end
